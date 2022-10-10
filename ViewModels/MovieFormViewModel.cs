ca9b92ca36842e14be6577572c50f5e2e8fd0ef4
AhmedMohamedAbdelhafez851 <102626727+AhmedMohamedAbdelhafez851@users.noreply.github.com>
2022-10-11 01:02:13 +0200

    Add files via upload

---
+++ ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CRUD_Operations.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_Operations.ViewModels
+{
+    public class MovieFormViewModel
+
+    {
+        public int Id { get; set; } 
+        [Required, StringLength(250)]
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+        [Range(1,10)]
+        public double Rate { get; set; }
+        [Required, StringLength(2500)]
+        public string StoreLine { get; set; }
+        public byte[] poster { get; set; }
+        [Display(Name ="Genre")]
+        public byte GenreId { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }   
+
+    }
+}
